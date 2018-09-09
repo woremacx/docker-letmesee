e7e0685ceed582aec769da396f91f9a33ff9513c
woremacx <woremacx@gmail.com>
2018-09-09 15:04:24 +0900

    import rubyeb19

---
+++ rubyeb19/extconf.rb
@@ -0,0 +1,30 @@
+require 'mkmf'
+
+have_library("z")
+have_library("intl") if /freebsd/ =~ RUBY_PLATFORM
+have_library('eb')
+have_func("rb_block_proc")
+have_func("eb_bitmap_to_png")
+have_header('eb/sysdefs.h')
+have_type('rb_encoding', ['ruby/ruby.h', 'ruby/encoding.h'])
+
+if have_func("eb_pthread_enabled")
+  print "checking that the EB library is pthread enabled... "
+  STDOUT.flush
+  if try_run(<<EOS)
+#include <eb/eb.h>
+
+int main()
+{
+    printf("eb_pthread_enabled() => %d\\n", eb_pthread_enabled());
+    return eb_pthread_enabled() ? 0 : 1;
+}
+EOS
+    puts "yes"
+    $defs << '-DRUBY_EB_ENABLE_PTHREAD'
+  else
+    puts "no"
+  end
+end
+
+create_makefile("eb")
